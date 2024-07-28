0ddd5ce59246ab7a9c0076e79a83198123176387
Kevin Qiu <kzqiu01@gmail.com>
2024-07-28 14:35:33 -0700

    Basic endpoints.

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, routing::post, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/health_check", get(health_check))
+        .route("/submit", post(submit_answer));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap()
 }
+
+async fn root() {}
+
+async fn health_check() -> String {
+    String::from("Hello world!")
+}
+
+async fn submit_answer() {}
